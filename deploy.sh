1e9d6e9e0b3379927526d700209d561e22a1c60f
Chase小念念 <65386393+Little-LinNian@users.noreply.github.com>
2021-05-12 17:18:46 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git clone -b dev https://github.com/YunYouJun/hexo-theme-yun themes/hexo-theme-yun && git clone https://github.com/Little-LinNian/blog-config && mv blog-config/_config.yml .&&yarn build&&yarn deploy
+git clone -b dev https://github.com/YunYouJun/hexo-theme-yun themes/hexo-theme-yun &&yarn build&&yarn deploy
