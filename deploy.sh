ff567c835d4a86b42703f415dcd228c932177b9a
Chase小念念 <65386393+Little-LinNian@users.noreply.github.com>
2021-05-12 20:51:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-git clone -b dev https://github.com/YunYouJun/hexo-theme-yun themes/hexo-theme-yun &&yarn build&&yarn deploy
+git clone -b dev https://github.com/YunYouJun/hexo-theme-yun themes/yun &&yarn build&&yarn deploy
