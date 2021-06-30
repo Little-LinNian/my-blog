f24aa390423914c4a40595dff81a9c9603c5b62b
Chase杨念 <7857257+chase_yang_nian@user.noreply.gitee.com>
2021-06-30 20:26:22 +0800

    qwq

--- source/js/wdnmd.js
+++ source/js/wdnmd.js
@@ -1,7 +1,6 @@
-if(sessionStorage.getItem("isReload")){
-    console.log("页面被刷新");
- }else{
-  console.log("首次被加载");
-  sessionStorage.setItem("isReload", true)
-  window.location.reload();
- }
+twikoo.init({
+    envId: 'blog-comment-3gi3d7fbf7cb76cb',
+    el: '#tcomment',
+    // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，如果您的环境地域不是上海，需传此参数
+    // path: 'window.location.pathname', // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
+  })
