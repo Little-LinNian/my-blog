c0d596ee6df3060f8d3c66b729f8020c6acd7d2d
Chase杨念 <7857257+chase_yang_nian@user.noreply.gitee.com>
2021-06-30 19:45:45 +0800

    qwq

--- source/js/wdnmd.js
+++ source/js/wdnmd.js
@@ -1,5 +1,14 @@
-function sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-  }
-location.reload();
-sleep(600)  
+try {
+    var reload_count = localStorage.getItem("reload_count");
+    if ( reload_count == 1 ){
+        location.reload();
+        reload_count++
+    }
+} catch(e) {
+    console.warn(e.message)
+    var reload_count = 1;
+
+}
+window.onbeforeunload = function () {
+    localStorage.setItem("reload_count",1)
+}
